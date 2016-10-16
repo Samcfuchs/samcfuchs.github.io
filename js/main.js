ea3a82c11cd06cb9e1aafa438c5805a877b8d2cb
Sam Fuchs <samcfuchs@gmail.com>
2016-10-16 18:29:02 -0400

    Clean up js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,6 @@
 var isLight = true;
 
 function switchTheme() {
-    console.log('Switching theme');
-    console.log(isLight);
-
     if (isLight) {
         goDark();
     } else {
@@ -15,27 +12,19 @@ function switchTheme() {
 
 function loadCSS(filename) {
     var fileref = document.createElement("link")
-    fileref.setAttribute("rel", "stylesheet")
-    fileref.setAttribute("type", "text/css")
-    fileref.setAttribute("href", filename)
+    fileref.setAttribute("rel", "stylesheet");
+    fileref.setAttribute("type", "text/css");
+    fileref.setAttribute("href", filename);
 
-    document.getElementsByTagName("head")[0].appendChild(fileref)
+    document.getElementsByTagName("head")[0].appendChild(fileref);
 }
 
 function goDark() {
     loadCSS("css/dark.css")
-    /*
-    if (!document.getElementById('css')) {
-        document.write('<link rel="stylesheet" type="text/css" href="css/dark.css">');
-    }*/
 }
 
 function goLight() {
     loadCSS("css/light.css")
-    /*
-    if (!document.getElementById('css')) {
-        document.write('<link rel="stylesheet" type="text/css" href="css/light.css">');
-    }*/
 }
 
 document.getElementById('circle').addEventListener('click', switchTheme);
