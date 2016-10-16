1d2fe66b82f4aa7af0d2ee0f9ebb56a6b20d2c0b
Sam Fuchs <samcfuchs@gmail.com>
2016-10-16 18:29:28 -0400

    Fix excessive generation of link elements

--- js/main.js
+++ js/main.js
@@ -15,15 +15,22 @@ function loadCSS(filename) {
     fileref.setAttribute("rel", "stylesheet");
     fileref.setAttribute("type", "text/css");
     fileref.setAttribute("href", filename);
+    fileref.setAttribute("class", "auto");
 
     document.getElementsByTagName("head")[0].appendChild(fileref);
 }
 
 function goDark() {
+    var head = document.getElementsByTagName("head")[0];
+    var css = document.getElementsByClassName('auto')[0];
+    head.removeChild(css);
     loadCSS("css/dark.css")
 }
 
 function goLight() {
+    var head = document.getElementsByTagName("head")[0];
+    var css = document.getElementsByClassName('auto')[0];
+    head.removeChild(css);
     loadCSS("css/light.css")
 }
 
